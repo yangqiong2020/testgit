8bf7e7cadbd942f4ca94947e23abf720b0a27cb1
yangqiong <364727769@qq.com>
2020-04-24 14:51:03 +0800

    develop1

--- com.one/test.java
+++ com.one/test.java
@@ -8,4 +8,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("这是主干------------");
     }
+
+    void develop(){
+        System.out.println("这是从新建的分支===========develop");
+    }
 }
