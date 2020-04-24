b92f2b751ec8c8548cdf4a7d7db0b481f8891b09
yangqiong <364727769@qq.com>
2020-04-24 15:05:43 +0800

    主干2

--- com.one/test.java
+++ com.one/test.java
@@ -8,4 +8,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("这是主干------------");
     }
+    void master2(){
+        System.out.println("这是主干2=============22");
+    }
 }
